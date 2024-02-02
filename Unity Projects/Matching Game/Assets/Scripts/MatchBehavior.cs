a72957b9efc74dc79f1be7729433f5f2e32d0a64
Kaitlyn Kinsey <kaitlynkinsey353@gmail.com>
2024-02-01 17:33:02 -0700

    Matching System
    Completed the matching system lesson

---
+++ Unity Projects/Matching Game/Assets/Scripts/MatchBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchBehavior : MonoBehaviour
+{
+    public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var tempObj = other.GetComponent<IDContainerBehavior>();
+        if(tempObj == null)
+            return;
+         
+        var otherID = tempObj.idObj;
+        if (otherID == idObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+        }
+    }
+}
